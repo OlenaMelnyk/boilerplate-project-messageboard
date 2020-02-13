8be7ac3e86f220fbc800944c6875579b0ceebe73
olena <melolena33@gmail.com>
2020-02-13 17:09:16 +0200

    Implement replies API. Implement functional testing.

--- routes/api.js
+++ routes/api.js
@@ -22,13 +22,16 @@ module.exports = function (app) {
   .put(reportThread)
   .delete(deleteThread);
 
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+  .get(repliesList)
+  .post(newReply)
+  .put(reportReply)
+  .delete(deleteReply);
 
   //threads
   function threadList(req, res) {
     mongo.connect(url, function(err, client) {
         expect(err, 'database error').to.not.exist;
-        console.log('Successful database connection');
         let database = client.db("test");
         var collection = database.collection(req.params.board);
         collection.find({},
@@ -39,12 +42,14 @@ module.exports = function (app) {
         .sort({bumped_on: -1})
         .limit(10)
         .toArray((err, docs) => {
-          docs.map((doc) => {
-            doc.replycount = doc.replies.length;
-            if (doc.replycount > 3) {
-              doc.replies = doc.replies.slice(-3);
-            }
-          });
+          if (docs) {
+            docs.map((doc) => {
+              doc.replycount = doc.replies.length;
+              if (doc.replycount > 3) {
+                doc.replies = doc.replies.slice(-3);
+              }
+            });
+          }
           res.json(docs);
         });
       });
@@ -83,7 +88,6 @@ module.exports = function (app) {
           {new: true, upsert: false},
           function(err, result){
             expect(err, 'database findAndModify error').to.not.exist;
-            console.log("res", result);
             res.json('reported');
           })
       });
@@ -95,7 +99,6 @@ module.exports = function (app) {
 
      mongo.connect(url, function(err, client) {
         expect(err, 'database error').to.not.exist;
-        console.log('Successful database connection');
         let database = client.db("test");
         var collection = database.collection(req.params.board);
 
@@ -118,6 +121,94 @@ module.exports = function (app) {
   }
 
   //replies
+  function repliesList(req, res) {
+    mongo.connect(url, function(err, client) {
+        expect(err, 'database error').to.not.exist;
+        let database = client.db("test");
 
+        var collection = database.collection(req.params.board);
+        collection.find({_id: new ObjectId(req.query.thread_id)},
+                       { delete_password : 0,
+                       reported: 0,
+                       "replies.reported": 0,
+                       "replies.delete_password": 0})
+        .toArray((err, docs) => {
+          res.json(docs[0]);
+        });
+      });
+  }
+
+  function newReply(req, res) {
+    //text, delete_password, & thread_id
+   let myReply = {
+      _id: new ObjectId(),
+      text : req.body.text,
+      created_on : new Date(),
+      reported : false,
+      delete_password: req.body.delete_password
+    };
+    mongo.connect(url, function(err, db) {
+      expect(err, 'database error').to.not.exist;
+      let database = db.db("test");
+      var collection = database.collection(req.params.board);
+          collection.findAndModify(
+          {
+            _id: new ObjectId(req.body.thread_id)
+           },
+          {},
+          {$push: { replies: myReply },
+            $set: {bumped_on: new Date()}},
+          {new: true, upsert: false},
+          function (err, doc) {
+            let redirectURL = '/b/' + req.params.board + '/' + req.body.thread_id;
+
+            res.redirect(redirectURL);
+          });
+    });
+  }
+
+  function reportReply(req, res) {
+      mongo.connect(url, function(err, client) {
+        expect(err, 'database error').to.not.exist;
+        let database = client.db("test");
+        var collection = database.collection(req.params.board);
+        collection.findAndModify(
+          {_id: new ObjectId(req.body.thread_id),
+          "replies._id" : new ObjectId(req.body.reply_id)},
+          {},
+          {$set: { "replies.$.reported": true }},
+          function(err, result){
+            expect(err, 'database findAndModify error').to.not.exist;
+            res.json('reported');
+          })
+      });
+  }
+
+  function deleteReply(req, res) {
+   let pass = req.body.delete_password;
+
+     mongo.connect(url, function(err, client) {
+        expect(err, 'database error').to.not.exist;
+        let database = client.db("test");
+        var collection = database.collection(req.params.board);
+
+         collection.findAndModify(
+           {
+             _id: new ObjectId(req.body.thread_id),
+             "replies._id": new ObjectId(req.body.reply_id),
+             "replies.delete_password": pass
+           },
+           {},
+           {$set : {"replies.$.text" : "[deleted]"}},
+          function(err, doc) {
+            if (doc.value == null) {
+                res.json("incorrect password");
+              } else {
+                res.json("success");
+              }
+            })
+     });
+
+  }
 
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,47 +13,197 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
+let threadID1;
+let threadID2;
+let replyID;
+
 suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
-    
+
     suite('POST', function() {
-      
+       test('Test POST /api/threads/:board', function(done) {
+        chai.request(server)
+        .post("/api/threads/general")
+        .send({text: "new",
+               delete_password: "1234"
+               })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+        });
+        chai.request(server)
+        .post("/api/threads/general")
+        .send({text: "new",
+               delete_password: "1234"
+               })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          done();
+        });
+       });
     });
-    
+
     suite('GET', function() {
-      
-    });
-    
+
+      test('Test GET /api/threads/:board', function(done) {
+        chai.request(server)
+        .get("/api/threads/general")
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body, "result body should be an array");
+          assert.isObject(res.body[0], "element of result body should be an object");
+          assert.property(res.body[0], '_id', "should contain _id property");
+          assert.property(res.body[0], 'text', "should contain text property");
+          assert.property(res.body[0], 'created_on', "should contain created_on property");
+          assert.property(res.body[0], 'bumped_on', "should contain bumped_on property");
+          assert.property(res.body[0], 'replies', "should contain replies property");
+          assert.isArray(res.body[0].replies, "replies should be an array");
+          assert.equal(res.body[0].text, 'new', "text should be 'new'");
+          threadID1 = res.body[0]._id;
+          threadID2 = res.body[1]._id;
+          done();
+        });
+      });
+  });
+
+
     suite('DELETE', function() {
-      
+      test('Test DELETE /api/threads/:board with incorrect password', function(done) {
+        chai.request(server)
+        .delete("/api/threads/general")
+        .send({thread_id: threadID1,
+               delete_password: "1111"})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, 'incorrect password', 'incorrect delete_password');
+          done();
+        });
+      });
+
+      test('Test DELETE /api/threads/:board with correct password', function(done) {
+        chai.request(server)
+        .delete("/api/threads/general")
+        .send({thread_id: threadID1,
+               delete_password: "1234"})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, 'success', 'incorrect delete_password');
+          done();
+        });
+      });
     });
-    
+
+
     suite('PUT', function() {
-      
+         test('Test PUT /api/threads/:board', function(done) {
+        chai.request(server)
+        .put("/api/threads/general")
+        .send({thread_id: threadID1})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "reported");
+          done();
+        });
+      });
     });
-    
+
 
   });
-  
+
   suite('API ROUTING FOR /api/replies/:board', function() {
-    
+
     suite('POST', function() {
-      
+      test('Test POST /api/replies/:board', function(done) {
+        chai.request(server)
+        .post("/api/replies/general")
+        .send({thread_id: threadID2,
+                text: "reply",
+               delete_password: "1111"
+               })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+        });
+        chai.request(server)
+        .post("/api/replies/general")
+        .send({text: "reply",
+               delete_password: "1111"
+               })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          done();
+        });
+       });
     });
-    
+
     suite('GET', function() {
-      
+     test('Test GET /api/threads/:board', function(done) {
+        chai.request(server)
+        .get("/api/replies/general")
+        .query({thread_id: threadID2})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "body should be an object");
+          assert.property(res.body, '_id', "should contain _id property");
+          assert.property(res.body, 'text', "should contain text property");
+          assert.property(res.body, 'created_on', "should contain created_on property");
+          assert.property(res.body, 'bumped_on', "should contain bumped_on property");
+          assert.property(res.body, 'replies', "should contain replies property");
+          assert.equal(res.body.text, 'new', "text should be 'new'");
+
+          assert.isArray(res.body.replies, "replies should be an array");
+          assert.property(res.body.replies[0], '_id', "should contain _id property");
+          assert.property(res.body.replies[0], 'text', "should contain text property");
+          assert.property(res.body.replies[0], 'created_on', "should contain created_on property");
+          assert.equal(res.body.replies[0].text, 'reply', "text should be 'reply'");
+          replyID = res.body.replies[0]._id;
+
+          done();
+        });
+      });
     });
-    
+
     suite('PUT', function() {
-      
+       test('Test PUT /api/replies/:board', function(done) {
+        chai.request(server)
+        .put("/api/replies/general")
+        .send({thread_id: threadID2,
+              reply_id: replyID})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, "reported");
+          done();
+        });
+      });
     });
-    
+
     suite('DELETE', function() {
-      
+        test('Test DELETE /api/replies/:board with incorrect password', function(done) {
+        chai.request(server)
+        .delete("/api/replies/general")
+        .send({thread_id: threadID2,
+               reply_id: replyID,
+               delete_password: "2222"})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, 'incorrect password', 'incorrect delete_password');
+          done();
+        });
+      });
+
+      test('Test DELETE /api/replies/:board with correct password', function(done) {
+        chai.request(server)
+        .delete("/api/replies/general")
+        .send({thread_id: threadID2,
+               reply_id: replyID,
+               delete_password: "1111"})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, 'success', 'incorrect delete_password');
+          done();
+        });
+      });
     });
-    
+
   });
 
 });
